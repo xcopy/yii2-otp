00cffd8572c0110a855cc817876771f77d1f7f37
Kairat Jenishev <kairat.jenishev@gmail.com>
2023-10-15 23:36:17 +0600

    Update migration

--- src/migrations/m231012_000001_add_opt_columns_to_user_table.php
+++ src/migrations/m231012_000001_add_opt_columns_to_user_table.php
@@ -12,9 +12,11 @@ class m231012_000001_add_opt_columns_to_user_table extends Migration
      */
     public function safeUp()
     {
-        $this->addColumn('user', 'otp', $this->string());
-        $this->addColumn('user', 'otp_expiry', $this->dateTime());
-        $this->addColumn('user', 'otp_token', $this->string());
+        $table = $this->getTable();
+
+        $this->addColumn($table, 'otp', $this->string());
+        $this->addColumn($table, 'otp_expiry', $this->dateTime());
+        $this->addColumn($table, 'otp_token', $this->string());
     }
 
     /**
@@ -22,8 +24,19 @@ class m231012_000001_add_opt_columns_to_user_table extends Migration
      */
     public function safeDown()
     {
-        $this->dropColumn('user', 'otp_token');
-        $this->dropColumn('user', 'otp_expiry');
-        $this->dropColumn('user', 'otp');
+        $table = $this->getTable();
+        
+        $this->dropColumn($table, 'otp_token');
+        $this->dropColumn($table, 'otp_expiry');
+        $this->dropColumn($table, 'otp');
+    }
+
+    /**
+     * @return string
+     */
+    private function getTable(): string
+    {
+        /** @see \yii\db\ActiveRecord::tableName() */
+        return call_user_func([Yii::$app->user->identityClass, 'tableName']);
     }
 }
